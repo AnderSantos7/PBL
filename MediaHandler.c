4b52e24558dd41a97987a9d99372b8d71b1422d5
AnderSantos7 <56338314+AnderSantos7@users.noreply.github.com>
2020-01-14 12:47:39 +0100

    La veldadela bueltodita bebe

---
+++ MediaHandler.c
@@ -0,0 +1,71 @@
+#include "SDL.h"
+#include "SDL_image.h"
+#include "SDL_mixer.h"
+#include "funtzioak.h"
+#include "objektuak.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+//enum SURFACE { SURFACE_SCREEN, SURFACE_BACKGROUND, SURFACE_PLAYER, SURFACE_EXTRA, SURFACE_ITEMS, SURFACE_LAND};
+SDL_Texture* loadMedia(char* src);
+
+SDL_Texture* loadMedia(char* src) {
+	SDL_Surface* s = NULL;
+	SDL_RWops* rwop;
+	rwop = SDL_RWFromFile(src, "rb");
+	s = IMG_LoadPNG_RW(rwop);
+	if (s == NULL) {
+		printf("Ezin izan da %s irudia kargatu. SDL_Error: %s\n", src, SDL_GetError());
+	}
+	SDL_Texture* t = SDL_CreateTextureFromSurface(renderer, s);
+
+	SDL_FreeSurface(s);
+	return t;
+}
+
+void aplikatuSurface(int x, int y, int w, int h, SDL_Texture* texture, SDL_Rect* clip) {
+	SDL_Rect offset;
+	offset.x = x;
+	offset.y = y;
+	offset.w = w;
+	offset.h = h;
+
+	SDL_RenderCopy(renderer, texture, clip, &offset);
+	return;
+}
+
+void loadFiles() {
+	textures[bgSurface] = loadMedia("assets/images/bg.png");
+	textures[playerSurface] = loadMedia("assets/images/player.png");
+	textures[itemsSurface] = loadMedia("assets/images/items.png");
+	textures[plantsSurface] = loadMedia("assets/images/sprite.png");
+	textures[pauseSurface] = loadMedia("assets/images/pause.png");
+	textures[homeSurface] = loadMedia("assets/images/home.png");
+	textures[obstacleSurface] = loadMedia("assets/images/obstacles.png");
+	textures[HUDSurface] = loadMedia("assets/images/HUD.png");
+	textures[firmaSurface] = loadMedia("assets/images/FIRMA.png");
+	textures[menuSurface] = loadMedia("assets/images/ANCIENTGARDEN.png");
+	
+	return;
+}
+
+void playPickUpSFX() {
+	char* src = "assets/sounds/pickUpSFX0.wav";
+	int random = rand() % 3;
+	switch (random) {
+	case 0: src = "assets/sounds/pickUpSFX0.wav"; break;
+	case 1: src = "assets/sounds/pickUpSFX1.wav"; break;
+	case 2: src = "assets/sounds/pickUpSFX2.wav"; break;
+	}
+	Mix_Chunk* pickUpSFX = Mix_LoadWAV_RW(SDL_RWFromFile(src, "rb"), 1);
+	Mix_PlayChannelTimed(-1, pickUpSFX, 0, -1);
+	return;
+}
+
+void playWellWaterSFX() {
+	char* src = "assets/sounds/wellWaterSFX.wav";
+	Mix_Chunk* waterSFX = Mix_LoadWAV_RW(SDL_RWFromFile(src, "rb"), 1);
+	Mix_PlayChannelTimed(-1, waterSFX, 0, -1);
+	//Mix_FreeChunk(waterSFX);
+	return;
+}
---
+++ tiles.c
@@ -0,0 +1,168 @@
+#include "SDL.h"
+#include "SDL_image.h"
+#include "funtzioak.h"
+#include "objektuak.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int i = 0;
+
+void updatePlants(int i, double deltaTime);
+void updateWater(int i, double deltaTime);
+
+void landatu(int seed) {
+	if (tiles[player.facingTile].plant.arado && tiles[player.facingDirection].plant.seed == NONE) {
+		tiles[player.facingTile].plant.seed = seed;
+	}
+	restaEnergy();
+
+	checkQuestCompletion(PLANT, seed, 1);
+	return;
+}
+
+void harvest(int tile) {
+	if (tiles[tile].plant.seed != 0 && tiles[tile].plant.stage == 2) {
+		int seedAmmount = 0, harvest = 0, seed = 0;
+		seedAmmount = rand() % 3 + 1;
+		harvest = seedToItem(tiles[tile].plant.seed);
+		seed = harvest + 1;
+		dropItem(tile, seed, seedAmmount);
+		dropItem(tile, harvest, 1);
+		tiles[tile].plant.seed = NONE;
+		tiles[tile].plant.stage = 0;
+		tiles[tile].plant.time = 0;
+
+		checkQuestCompletion(HARVEST, harvest, 1);
+	}
+}
+
+void water(int tile) {
+	tiles[tile].plant.water = 1;
+	tiles[tile].plant.lastWater = 0;
+	restaEnergy();
+
+	checkQuestCompletion(WATER, tiles[tile].plant.seed, 1);
+	return;
+}
+
+void arar(int tile) {
+	tiles[tile].plant.arado = 1;
+	restaEnergy();
+
+	checkQuestCompletion(ARAR, 0, 1);
+	return;
+}
+
+void fertilize(int tile) {
+	tiles[tile].plant.time += 100;
+
+	checkQuestCompletion(FERTILIZAR, tiles[tile].plant.seed, 1);
+	return;
+}
+
+int getFacingTileId() {
+	int ID = player.tile;
+	switch (player.facingDirection) {
+	case DIR_LEFT:
+		ID = player.tile - 1;
+		break;
+	case DIR_RIGHT:
+		ID = player.tile + 1;
+		break;
+	case DIR_UP:
+		ID = player.tile - 16;
+		break;
+	case DIR_DOWN:
+		ID = player.tile + 16;
+		break;
+	}
+	return ID;
+}
+
+
+struct posCoord getTilePosFromId(int ID) {
+	struct posCoord pos;
+	pos.x = ID % GRID_SIZE;
+	pos.y = ID / GRID_SIZE;
+	return pos;
+}
+
+int getTileFromPos(int x, int y) {
+	int ID = 0;
+	ID = (y / 64) * 16 + (x / 64);
+	return ID;
+}
+
+void marraztuTiles() {
+	int i = 0;
+	SDL_Rect pos;
+	SDL_Rect clip;
+	clip.w = TILE_SIZE;
+	clip.h = TILE_SIZE;
+	for (i = 0; i < 49; i++) {
+		pos.x = tiles[plantable_ID[i]].x * TILE_SIZE;
+		pos.y = tiles[plantable_ID[i]].y * TILE_SIZE;
+		if (tiles[plantable_ID[i]].plant.arado) {
+			switch (tiles[plantable_ID[i]].plant.seed) {
+			case NONE:
+				clip.x = tiles[plantable_ID[i]].plant.water * TILE_SIZE;
+				clip.y = 0;
+				break;
+			default:
+				clip.x = (tiles[plantable_ID[i]].plant.stage + 3 * tiles[plantable_ID[i]].plant.water) * TILE_SIZE;
+				clip.y = tiles[plantable_ID[i]].plant.seed * TILE_SIZE;
+			}
+			aplikatuSurface(pos.x - camera.x, pos.y - camera.y, 64, 64, textures[plantsSurface], &clip);
+		}
+		if (plantable_ID[i] == player.facingTile) {
+			clip.x = 2 * TILE_SIZE;
+			clip.y = 0;
+			aplikatuSurface(pos.x - camera.x, pos.y - camera.y, 64, 64, textures[plantsSurface], &clip);
+		}
+	}
+}
+
+void updateTiles(double deltaTime) {
+	for (int i = 0; i < 49; i++) {
+		if (tiles[plantable_ID[i]].plant.arado) {
+			updatePlants(i, deltaTime * 10);
+			updateWater(i, deltaTime);
+		}
+	}
+	return;
+}
+
+void updatePlants(int i, double deltaTime) {
+	if (tiles[plantable_ID[i]].plant.seed != NONE) {
+		tiles[plantable_ID[i]].plant.time += deltaTime;
+		if (tiles[plantable_ID[i]].plant.time > 50) {
+			if (tiles[plantable_ID[i]].plant.time > 100) {
+				tiles[plantable_ID[i]].plant.stage = 2;
+			}
+			else {
+				tiles[plantable_ID[i]].plant.stage = 1;
+			}
+		}
+	}
+	return;
+}
+
+void updateWater(int i, double deltaTime) {
+	if (tiles[plantable_ID[i]].plant.water) {
+		tiles[plantable_ID[i]].plant.lastWater += deltaTime;
+		if (tiles[plantable_ID[i]].plant.lastWater > 50) {
+			tiles[plantable_ID[i]].plant.water = 0;
+			tiles[plantable_ID[i]].plant.lastWater = 0;
+		}
+	}else if (!tiles[plantable_ID[i]].plant.water) {
+		tiles[plantable_ID[i]].plant.lastWater += deltaTime;
+		if (tiles[plantable_ID[i]].plant.lastWater >= 100) {
+			tiles[plantable_ID[i]].plant.time = 0;
+			tiles[plantable_ID[i]].plant.seed = NONE;
+			tiles[plantable_ID[i]].plant.arado = 0;
+			tiles[plantable_ID[i]].plant.lastWater = 0;
+			tiles[plantable_ID[i]].plant.stage = 0;
+		}
+	}
+	return;
+}
